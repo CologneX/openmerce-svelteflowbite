8588208f95d746ecca5328f0a5920d26972cdede
Kyrell <kleanosiauw@student.ciputra.ac.id>
2023-07-03 00:45:20 +0700

    feat(login):login page UI done. TODO: make gradient background, set max size and padding

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,26 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+    namespace App {
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
+        // interface Platform {}
+    }
 }
 
-export {};
+declare interface LoginFormData {
+    email: string;
+    password: string;
+    remember_me: boolean;
+}
+
+declare interface RegisterFormData {
+    first_name: string;
+    last_name: string;
+    gender: string;
+    birth_date: Date;
+    email: string;
+    password: string;
+}
+
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-    import {ActionIcon, Anchor, Button, Card, Center, Checkbox, Divider, Text, TextInput} from "@svelteuidev/core";
+
     import Logo from "$lib/icons/Logo.svelte";
     import Mail from "$lib/icons/Mail.svelte";
+    import {Button, ButtonGroup, Card, Checkbox, Input, InputAddon} from "flowbite-svelte";
     import Lock from "$lib/icons/Lock.svelte";
-    import Eye from "$lib/icons/Eye.svelte";
-
 
     // form bind values
     let isLoggingIn = false
+
+
     let loginData: LoginFormData = <LoginFormData>{
         email: "",
         password: ""
@@ -65,60 +66,38 @@
     <meta name="description" content="Login to your OpenMerce account"/>
 </svelte:head>
 
-<Center class="bg-contain bg-no-repeat bg-center w-full h-full" style="background-image: url('/login-gradient.png')">
-    <div class="w-full max-w-xs drop-shadow-xl">
+<div class="w-full h-full grid place-content-center">
+    <p class="w-full max-w-xs drop-shadow-xl">
         <Logo/>
-        <div class="h-28"></div>
-        <Card class="space-y-4 ">
-            <!--            <InputWrapper label="E-mail" description="Please enter your email here"-->
-            <!--            >-->
-            <!--                <TextInput icon="{Mail}" />-->
-            <!--            </InputWrapper>-->
-            <!--            <InputWrapper label="Password" description="Enter your very secure password"-->
-            <!--            >-->
-            <!--                <TextInput icon="{Lock}"/>-->
-            <!--            </InputWrapper>-->
-            <TextInput
-                    label="E-mail"
-                    description="Please enter your email here"
-                    type="email"
-                    icon="{Mail}"
-                    error={loginDataErrors.email}
-                    bind:value={loginData.email}
-                    required
-            />
-            <TextInput
-                    label="Password"
-                    description="Enter your very secure password"
-                    {type}
-                    icon="{Lock}"
-                    error={loginDataErrors.password}
-                    bind:value={loginData.password}
+    <div class="h-28"></div>
+    <Card class="space-y-4 w-full max-w-xs">
+        <ButtonGroup class="w-full">
+            <InputAddon>
+                <Mail/>
+            </InputAddon>
+            <Input placeholder="E-mail" bind:value={loginData.email}
+                   required/>
+        </ButtonGroup>
+        <ButtonGroup class="w-full">
+            <InputAddon>
+                <Lock/>
+            </InputAddon>
+            <Input placeholder="Password" bind:value={loginData.password}
 
-                    required
-            >
-                <svelte:fragment slot='rightSection'>
-                    <ActionIcon size="xs" on:click={()=> show_password = !show_password}>
-                        <Eye/>
-                    </ActionIcon>
-                </svelte:fragment>
-            </TextInput>
-            <Checkbox label="Remember me" size="sm"/>
-            <Button fullSize
-                    ripple
-                    loaderPosition="right"
-                    loading={isLoggingIn}
-                    disabled={(loginDataErrors.password ||  loginDataErrors.email) || (loginData.email === "" || loginData.password === "")}
-                    class="disabled:bg-gray-500 transition-colors duration-300 ease-in">
-                Login
-            </Button>
-            <Divider/>
-            <Text class="text-center" size="sm">Don't have an account?
-                <Anchor href="/register" size="sm" weight={"semibold"}>Register</Anchor>
-            </Text>
-            <Text class="text-center" size="xs">Need help?
-                <Anchor href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" size="xs">OpenMerce HelpDesk</Anchor>
-            </Text>
-        </Card>
-    </div>
-</Center>
+                   required/>
+        </ButtonGroup>
+        <Checkbox>Remember me</Checkbox>
+        <Button
+                disabled={(loginDataErrors.password ||  loginDataErrors.email) || (loginData.email === "" || loginData.password === "")}
+                class="disabled:bg-gray-500 transition-colors duration-300 ease-in">
+            Login
+        </Button>
+        <div class="divider-horizontal"></div>
+        <p class="text-center">Don't have an account?
+            <a href="/register" class="font-semibold">Register</a>
+        </p>
+        <p class="text-center">Need help?
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="font-semibold">OpenMerce HelpDesk</a>
+        </p>
+    </Card>
+</div>
