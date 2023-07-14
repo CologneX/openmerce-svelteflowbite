76298f11b106d55ddfcd1556dc74a41dde6e2167
Kyrell <kleanosiauw@student.ciputra.ac.id>
2023-07-15 02:22:19 +0800

    fix(transaction-list): text color for dark and light mode

--- src/routes/(user)/transaction-list/+page.svelte
+++ src/routes/(user)/transaction-list/+page.svelte
@@ -5,7 +5,7 @@
     import ClockMini from "$lib/icons/ClockMini.svelte";
     import Next from "$lib/icons/Next.svelte";
     import CancelMini from "$lib/icons/CancelMini.svelte";
-    import {Badge, Button, Input} from "flowbite-svelte";
+    import {Badge, Button, Heading, Input, P} from "flowbite-svelte";
 
     let products = [
         {name: 'Product 1', status: 'pending'},
@@ -32,7 +32,7 @@
 
 <div class="space-y-8">
     <div class="space-y-2">
-        <h4>Transaction List</h4>
+        <Heading tag="h4">Transaction List</Heading>
         <div class="flex flex-row gap-x-2">
             <Input icon={Search} placeholder="Search transactions" class="w-full flex-1"/>
             <Button variant="outline" ripple>
@@ -41,7 +41,7 @@
             </Button>
         </div>
         <div class="flex flex-row overflow-x-auto no-scrollbar gap-x-2 py-1">
-            <p class="text-sm md:text-base font-semibold my-auto">Filter</p>
+            <P class="text-sm md:text-base font-semibold my-auto">Filter</P>
             <Button variant="subtle" size={$screenWidthStore < 400? 'xs': 'sm'} on:click={handleResetFilter}>
                 <CancelMini/>
             </Button>
@@ -74,7 +74,7 @@
             </Button>
         </div>
         <Button class="w-full grid grid-cols-2" outline>
-            <div class="flex justify-start">
+            <div class="flex justify-start items-center">
                 <ClockMini/>
                 <span class="text-xs md:text-sm">&nbsp; Pending Payment</span>
             </div>
@@ -85,35 +85,34 @@
     </div>
     <div class="flex flex-col space-y-4">
         {#each products as mantap}
-            <div class="shadow-md rounded-md p-4">
-                <p class="truncate flex justify-between sm:justify-start text-xs md:text-sm pb-2">
+            <div class="shadow-md rounded-md p-4 dark:bg-gray-700">
+                <P class="truncate flex justify-between sm:justify-start text-xs md:text-sm pb-2">
                     <span class="font-bold pr-4">16 Oct 2022</span>
-
                     <Badge size={$screenWidthStore < 400? 'xs': 'sm'} radius="sm"
                            color={mantap.status === 'pending'?'yellow': mantap.status==='cancelled'?'red':'blue'}>
                         {mantap.status}
                     </Badge>
-                </p>
+                </P>
                 <div class="flex flex-row gap-x-4">
                     <img src="/emptycart.png" alt="gambar" class="object-contain aspect-square h-full max-h-20">
 
                     <div class="truncate grid grid-flow-col w-full">
                         <div class="truncate">
-                            <p class="truncate text-sm md:text-base">
+                            <P class="truncate text-sm md:text-base">
                                 {mantap.name} awdojahdiagduadadaweawarer earsef
-                            </p>
-                            <p class="text-xs md:text-sm">Rp. 69</p>
+                            </P>
+                            <P class="text-xs md:text-sm">Rp. 69</P>
                         </div>
                         <div class="justify-self-end sm:pr-10 pl-2 hidden sm:block">
-                            <span class=" text-xs md:text-sm text-neutral-500">Total Price</span>
-                            <p class="font-bold text-sm md:text-base">Rp. 0</p>
+                            <span class=" text-xs md:text-sm text-neutral-400">Total Price</span>
+                            <P class="font-bold text-sm md:text-base">Rp. 0</P>
                         </div>
                     </div>
                 </div>
                 <div class="grid grid-cols-2 sm:grid-cols-1 ">
                     <div class="sm:pr-10 pl-2 sm:hidden">
-                        <p class="text-neutral-500">Total Price</p>
-                        <p class="font-bold text-sm md:text-base">Rp. 0</p>
+                        <span class="text-xs text-neutral-400">Total Price</span>
+                        <P class="font-bold text-sm md:text-base">Rp. 0</P>
                     </div>
 
                     <div class="justify-end flex items-center {$screenWidthStore > 1024 ? 'gap-x-5' : 'gap-x-2'}">
@@ -126,7 +125,7 @@
                                 Review
                             </Button>
                         {/if}
-                        <Button size={$screenWidthStore < 400? 'xs': 'sm'}  class="border-none h-fit" outline>
+                        <Button size={$screenWidthStore < 400? 'xs': 'sm'} class="border-none h-fit" outline>
                             Detail
                         </Button>
                     </div>
